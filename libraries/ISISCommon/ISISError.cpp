3dfe35fedc3ff91519b2e7d40032df8fe1e75e62
Kathryn Baker <KathrynBaker@users.noreply.github.com>
2018-07-03 16:33:41 +0100

    Add comments to explain code

--- libraries/ISISCommon/ISISError.cpp
+++ libraries/ISISCommon/ISISError.cpp
@@ -1,23 +1,31 @@
+/*
+ * ISISError.cpp - Code for managing Errors
+ */
 #include "Arduino.h"
 #include "ISISError.h"
 #include "ISISSerial.h"
 
+// Use the isis namespace
 namespace  isis {
+  // Create an error class which is empty, do not use clear as the operations may be different shuold there be a need to persist errors in the future
   ISISError::ISISError() {
     _errorState = true;
     _errorMessage = "No error";
   }
 
+  // "Display" the error to the serial line
   void ISISError::errorDisplay( void ){
     isis::serialWriteLine(_errorMessage);
   }
 
+  // Update the error so that it is in an error state, and immediatly write the error to the serial line
   void ISISError::setError(String reason) {
     _errorState = true;
     _errorMessage = reason;
     isis::serialWriteLine("ERROR");
   }
 
+  // Clear any errors
   void ISISError::clearError() {
     _errorState = true;
     _errorMessage = "No error";
