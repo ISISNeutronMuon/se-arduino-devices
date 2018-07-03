6b6a3a5e751464933103560cc9f49ffa0456aeb2
Kathryn Baker <KathrynBaker@users.noreply.github.com>
2018-07-03 16:41:44 +0100

    Add comments to explain code

--- libraries/ISISCommon/ISISIO.cpp
+++ libraries/ISISCommon/ISISIO.cpp
@@ -1,18 +1,26 @@
+/*
+ * ISISIO.cpp - Code for input and output
+ */
 #include "Arduino.h"
 
+// Use the ISIS namespace
 namespace  isis {
+  // Set specified pin to be an output
   void setDigitalOut(int pin) {
     pinMode(pin, OUTPUT);
   }
 
+  // Set specified pin to be an input
   void setDigitalIn(int pin) {
     pinMode(pin, INPUT);
   }
 
+  // Convert returned pin value to a TRUE or FALSE string, 1 = TRUE, 0 = FALSE
   String readDigitalString(int pin) {
     return (digitalRead(pin) ? "TRUE" : "FALSE");
   }
 
+  // Set pin to HIGH/LOW based on a TRUE or FALSE string, 1 = TRUE, 0 = FALSE
   void writeDigitalString(int pin, String state) {
     if (state == "TRUE") {
       digitalWrite(pin, HIGH);
@@ -22,10 +30,12 @@ namespace  isis {
     }
   }
 
+  // Convert returned pin value to a TRUE or FALSE string, inverted, 1 = FALSE, 0 = TRUE
   String readDigitalStringInvert(int pin) {
     return (digitalRead(pin) ? "FALSE" : "TRUE");
   }
 
+  // Set pin to HIGH/LOW based on a TRUE or FALSE string, 1 = FALSE, 0 = TRUE
   void writeDigitalStringInvert(int pin,String state) {
     if (state == "TRUE") {
       digitalWrite(pin, LOW);
