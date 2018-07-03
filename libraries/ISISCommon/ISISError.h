3f1a344969c3a2bb72d88ad87da9f9f0ac12e3c6
Kathryn Baker <KathrynBaker@users.noreply.github.com>
2018-07-03 16:36:51 +0100

    Add comments to explain code

--- libraries/ISISCommon/ISISError.h
+++ libraries/ISISCommon/ISISError.h
@@ -1,18 +1,28 @@
+/*
+ * ISISError.h - Header for the ISIS Error class
+ */
+// Only define the header if it hasn't been defined previously
 #ifndef HEADER_ISISERROR
+  // Define the header information
   #define HEADER_ISISERROR
 
+  // Use the ISIS namespace
   namespace  isis {
+    // Declare the Error class
     class ISISError {
       public:
+        // Initialise error class values
         ISISError();
+        // Functions to perform on the error
         void errorDisplay( void );
         void setError(String reason);
         void clearError();
       private:
+        // Variables kept private
         String _errorMessage;
         boolean _errorState;
     };
   
   } // end namespace isis
    
-#endif
+#endif // end if header not defined
